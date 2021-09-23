1dfa64de793deb0791f1ed10b5ceb9120420fc48
Shashwat Sahai <ssahai97@gmail.com>
2021-09-24 01:55:45 +0530

    add files

---
+++ src/Components/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+    Maincontainer: require("./Maincontainer").default,
+    Navbar: require("./Navbar").default,
+    Contact: require("./Contact").default,
+    Projects: require("./Projects").default
+}
